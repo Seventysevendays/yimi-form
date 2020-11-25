7276f8aa42a7737a305e0df44b2b6ce6c12617c2
77days <867789352@qq.com>
2020-11-25 10:08:15 +0800

    feat: yimi schema form

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,11 +1,31 @@
 module.exports = {
   docs: [
-    { type: 'category', label: 'Home', items: ['home/home'], collapsed: true },
-    { type: 'category', label: 'Overview', items: ['overview/graceful', 'overview/highPerformance'], collapsed: true },
-    { type: 'category', label: 'Usage', items: ['usage/basic', 'usage/components'], collapsed: true },
+    { type: "category", label: "Home", items: ["home/home"], collapsed: true },
     {
-      type: 'category', label: 'API', items: [
-        'api/core', 'api/form', 'api/formItem', 'api/arrayList', 'api/arrayTable', 'api/arrayAction'], collapsed: true
-    }
-  ]
+      type: "category",
+      label: "Overview",
+      items: ["overview/graceful", "overview/highPerformance"],
+      collapsed: true,
+    },
+    {
+      type: "category",
+      label: "Usage",
+      items: ["usage/basic", "usage/components", "usage/schemaForm"],
+      collapsed: true,
+    },
+    {
+      type: "category",
+      label: "API",
+      items: [
+        "api/core",
+        "api/form",
+        "api/formItem",
+        "api/arrayList",
+        "api/arrayTable",
+        "api/arrayAction",
+        "api/schemaForm",
+      ],
+      collapsed: true,
+    },
+  ],
 };
