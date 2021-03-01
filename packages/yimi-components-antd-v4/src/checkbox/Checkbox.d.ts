8c6513ec38b6293f448ccd4d887acf90e72c2eaa
77days <867789352@qq.com>
2021-03-01 10:21:23 +0800

    fix: checkbox ts

--- packages/yimi-components-antd-v4/src/checkbox/Checkbox.d.ts
+++ packages/yimi-components-antd-v4/src/checkbox/Checkbox.d.ts
@@ -2,7 +2,7 @@ import React from "react";
 import { CheckboxProps as ACheckboxProps } from "antd/lib/checkbox";
 import { Status } from "../types";
 interface CheckboxProps extends ACheckboxProps {
-    status: Status;
+    status?: Status;
 }
 declare class Checkbox extends React.Component<CheckboxProps> {
     render(): JSX.Element;
