f7de318bda7c1f2887dacc5da8477042c2753eeb
77days <867789352@qq.com>
2021-10-18 10:59:52 +0800

    feat: arraylist index

--- packages/yimi-components/src/index.d.ts
+++ packages/yimi-components/src/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="react" />
 export { default as ArrayTable } from "./ArrayTable/ArrayTable";
 export { default as ArrayList } from "./ArrayList/ArrayList";
 export declare const ArrayAction: import("react").Consumer<import("./context/arrayTable").ArrayTableActionValue>;
+export declare const ArrayIndex: import("react").Consumer<import("./context/arrayIndex").ArrayIndexValue>;
--- packages/yimi-components/src/index.ts
+++ packages/yimi-components/src/index.ts
@@ -1,4 +1,6 @@
 import ArrayContext from "./context/arrayTable";
+import ArrrayIndexContext from './context/arrayIndex'
 export { default as ArrayTable } from "./ArrayTable/ArrayTable";
 export { default as ArrayList } from "./ArrayList/ArrayList";
 export const ArrayAction = ArrayContext.Consumer;
+export const ArrayIndex = ArrrayIndexContext.Consumer;
